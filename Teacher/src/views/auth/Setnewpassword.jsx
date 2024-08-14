8d5f03cec595ee7ca99dfbb3ae518e2f6aa83e2b
Kraftwerk <ibrahim.soft.dev23@gmail.com>
2024-08-14 19:00:29 +0600

    update

--- Teacher/src/views/auth/Setnewpassword.jsx
+++ Teacher/src/views/auth/Setnewpassword.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../assets/Teacher/logo1.png';
 import teacherImage from '../../assets/Teacher/resetpass.svg';
 import { Link } from 'react-router-dom';
-
+import { CiLock} from "react-icons/ci";
 function ForgetPasswordPage() {
     return (
         <div className="fixed inset-0 bg-cover bg-[#A7BEAE]">
@@ -24,17 +24,29 @@ function ForgetPasswordPage() {
                         <div className='w-[446px] h-[115px] items-center justify-center text-center'>
                         <h2 className="text-[24px] text-gray-700 mb-6 mt-10 font-bold">Reset Password</h2>
                         </div>
-                         
-                        <input
-                            type="email"
+                         <div>
+                         <label className="text-gray-700 mb-2 flex items-center">
+          <CiLock className="text-[#BB5042] w-6 h-6 mr-2" />
+          New Password
+        </label>
+        <input
+                            type="password"
                             placeholder="New password"
                             className="w-[446px] p-3 border border-gray-300 rounded-lg mb-4"
                         />
-                         <input
-                            type="email"
+                         </div>
+                        <div>
+                        <label className="text-gray-700 mb-2 flex items-center">
+          <CiLock className="text-[#BB5042] w-6 h-6 mr-2" />
+          Confrim Password
+        </label>
+        <input
+                            type="password"
                             placeholder="Confirm Password"
-                            className="w-[446px] mt-5 p-3 border border-gray-300 rounded-lg mb-4"
+                            className="w-[446px] mt-2 p-3 border border-gray-300 rounded-lg mb-4"
                         />
+                        </div>
+                       
                         <Link to='/'>
                         <button className="w-[446px] mt-10 mb-12 p-3 bg-[#BB5042] text-white rounded-lg">
                             Confirm
